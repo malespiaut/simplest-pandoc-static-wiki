507bd783f4d040924a465c25607c436abe72da4d
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2022-12-18 13:36:03 +0100

    Adding .gitignore to ignore the out/ directory. The directory is generated when running build.sh.

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-cd src
-for i in $(ls)
+# Making the out/ directory
+mkdir -p out
+
+# Moving into the src/ directory; exiting if `cd` fails
+cd src || exit
+
+# For each files in the src/ directory, run the Pandoc command
+for file in *
 do
-  pandoc --standalone --from mediawiki --to html --output ../out/"$i" "$i"
+  [[ -e "$file" ]] || break  # Handles the case of no files
+  pandoc --standalone --table-of-content --from mediawiki --to html --output ../out/"$file" "$file"
+  echo "$file"
 done
 
-cd ../out
+# Moving into the out/ directory; exiting if `cd` fails
+cd ../out || exit
+
+# Renaming index to index.html, since file extension is required only for the index page.
 mv index index.html
 
 echo "Done!"
